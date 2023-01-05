25eb4df9dc6d4c3f7f375fd407be810589d97ff8
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-05 14:37:34 +0900

    [Feat] ContentView.swift 네트워크 통신 테스트

--- starcket/Sources/Views/ContentView.swift
+++ starcket/Sources/Views/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+	@StateObject var bucketStore = BucketStore()
+	
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+			ScrollView {
+				ForEach(bucketStore.bucket) { bucket in
+					Text(bucket.id)
+					Text(bucket.userId)
+					Text(bucket.detailId.first ?? "")
+					Text(bucket.icon)
+					Text(bucket.title)
+					Text(bucket.isCheck ? "true" : "false")
+					Text("\(bucket.pos.first!)")
+					Text("\(bucket.shape)")
+					VStack {
+						Text("\(bucket.updatedAt)")
+						Text("\(bucket.createdAt)")
+						Button {
+							let bucket = Bucket(id: UUID().uuidString, userId: "7BW5aWDlcP8E5NllOu4f", detailId: [], icon: "✈️", title: "제주도 여행가기", isCheck: false, pos: [0.0, 0.0], shape: 0, updatedAt: Date(), createdAt: Date())
+							bucketStore.createBucket(bucket)
+						} label: {
+							Text("추가")
+						}
+
+					}
+				}
+			}
         }
         .padding()
+		.onAppear {
+			Task {
+				UserDefaults.standard.set("7BW5aWDlcP8E5NllOu4f", forKey: "userIdToken")
+				(bucketStore.bucket, bucketStore.bucketIdList) = try await bucketStore.fetchBucket()
+			}
+		}
     }
 }
 
