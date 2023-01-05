d18e102e2ab66cb705bd8add78b2b654efdf47cf
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-05 09:56:44 +0900

    [Feat] Constants(제약사항) 설정

---
+++ starcket/Configuration/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  starcket
+//
+//  Created by geonhyeong on 2023/01/05.
+//
+
+import Foundation
+import SwiftUI
+
+struct Screen {
+	static let maxWidth = UIScreen.main.bounds.width
+	static let maxHeight = UIScreen.main.bounds.height
+}
