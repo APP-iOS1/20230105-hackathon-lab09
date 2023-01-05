c7c7d4123dff2c8633d1158bfb0d1a149959c1ae
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-05 13:15:24 +0900

    [Feat] User Model 생성

---
+++ starcket/Sources/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  starcket
+//
+//  Created by geonhyeong on 2023/01/05.
+//
+
+import Foundation
+
+struct User: Codable, Identifiable {
+	let id: String			// 문서 id
+	let bucketId: [String]	// bucket id
+	let detailId: [String]	// depth1, bucketdetail들의 id
+	var name: String		// 닉네임
+	var email: String		// 이메일
+	var isPremium: Bool		// 프리미엄 여부
+	var updateAt: Date		// 수정일
+	let createdAt: Date		// 생성일
+}
