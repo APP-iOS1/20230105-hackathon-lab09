1f26d80baebfaec87eac8895565dc182f74abcb6
sue <suekim999@naver.com>
2023-01-05 21:38:39 +0900

    [Add] 마이페이지 추가

--- starcket/Sources/Views/MyPage/MyPageView.swift
+++ starcket/Sources/Views/MyPage/MyPageView.swift
@@ -11,14 +11,23 @@ struct MyPageView: View {
     @State var faceIDToggle: Bool = false
     @State var darkmodeToggle: Bool = false
     @State var pushNotiToggle: Bool = false
+    @State var logOutAlert: Bool = false
     var body: some View {
         NavigationStack {
             List {
-                NavigationLink {
-                    MoreView()
-                } label: {
-                    Text("hi")
-                    
+                HStack {
+                    Image("profileStar")
+                        .resizable()
+                        .frame(width: 45, height: 42)
+                        .padding(.trailing, 15)
+                    VStack (alignment: .leading) {
+                        Spacer()
+                        Text("여구름")
+                        Text("hihistar@naver.com")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                        Spacer()
+                    }
                 }
                 Section (header: Text("Setting") ) {
                     HStack{
@@ -35,7 +44,7 @@ struct MyPageView: View {
                     }//hstack
                     
                     NavigationLink {
-                        
+                        PurchasePremiumView()
                     } label: {
                         Text("프리미엄 구매")
                     }
@@ -49,7 +58,7 @@ struct MyPageView: View {
                     }
                     //로그아웃 -> alert 로그아웃 하시겠습니까?
                     Button {
-                        
+                        logOutAlert.toggle()
                     } label: {
                         Text("로그아웃")
                         
@@ -68,6 +77,26 @@ struct MyPageView: View {
 
             }
         }
+        
+        .alert("", isPresented: $logOutAlert) {
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("아니요")
+                }
+                Button {
+                    
+                } label: {
+                    Text("예")
+                }
+
+            }
+        } message: {
+            Text("로그아웃 하시겠어요?")
+
+        }
+
     }
 }
 
