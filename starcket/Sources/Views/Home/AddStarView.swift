edddd06f36ef2a3a06309cb291f8d0f86df1d486
sohee120 <yuhsohee120@gmail.com>
2023-01-05 21:00:06 +0900

    [Add] 별 에셋 추가

--- starcket/Sources/Views/Home/AddStarView.swift
+++ starcket/Sources/Views/Home/AddStarView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct AddStarView: View {
 //    @Environment(\.presentationMode) var addingStar // dismiss
-    var stars = [0, 0, 0, 0, 0] // DB의 Bucket의 shape 값
-    var starNum:Int = 0 // DB의 Bucket의 isCheck가 true인 거 개수
     var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    @State private var starPosList: [CGSize] = [] // 드래그한 만큼 별이 움직이도록 binding에 사용될 Property
+    @State private var accumlatedOffset: [CGSize] = [] // 지금까지 드래그 된 값을 기록하고 있는 Property
+    //@Binding var waitingStarList: [Bucket]
+    // waitingStarList = [11.1, 12.1, 12.23, 12.25]
+    // waitingStarList에서 빼줘야할것같음
+    // 별 버튼을 누르면 그 별의 isFloating값을 true로 해줘야함
+    // shape값 넘겨줘야함 - update함수를 통해서 ?
+    @StateObject var bucketStore: BucketStore = BucketStore()
     var body: some View {
         VStack{
             HStack{
-                Text("추가할 수 있는 별이 \(starNum)개 있습니다.")
+                Text("추가할 수 있는 별이 \(0)개 있습니다.")
                     .font(.title3)
                     .padding()
                 Spacer()
@@ -29,11 +35,12 @@ struct AddStarView: View {
             }
             ScrollView {
                 LazyVGrid(columns: columns) {
-                    ForEach((0..<stars.count), id: \.self) { i in
+                    ForEach((0..<8), id: \.self) { i in
                         Button {
-                            
+                            starPosList.append(.zero)
+                            accumlatedOffset.append(.zero)
                         } label: {
-                            Image("\(0)")
+                            Image("\(i)")
                                 .resizable()
                                 .frame(width: 100, height: 100)
                         }
