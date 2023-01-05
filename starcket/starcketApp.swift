7533828807eb23937267208292daa55fa08709b4
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-05 09:56:24 +0900

    [Init] Project 시작

---
+++ starcket/starcketApp.swift
@@ -0,0 +1,17 @@
+//
+//  starcketApp.swift
+//  starcket
+//
+//  Created by geonhyeong on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct starcketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
