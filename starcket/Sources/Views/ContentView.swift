8475f8436d6955c98b24d60f555302aa50b71057
Jun Shin <mnhfn8hj@gmail.com>
2023-01-06 14:41:59 +0900

    [Feat] 스프래쉬 기능 추가

--- starcket/Sources/Views/ContentView.swift
+++ starcket/Sources/Views/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     @State private var tabSelection: Int = 0
+    
+    @State var animate = false
+    @State var endSplash = false
     
     
     var body: some View {
-//        NavigationView {
-//            SignInView()
+
+        ZStack{
             TabView(selection: $tabSelection) {
                 HomeView()
                     .tabItem {
@@ -35,46 +40,47 @@ struct ContentView: View {
                         Image(systemName: "person.crop.circle.fill")
                         Text("마이페이지")
                     }.tag(3)
-
                 
-        
-//                // 수현 테스트용
-//                SoohyunTest()
-//                    .tabItem {
-//                        Image(systemName: "star")
-//                        Text("실험용")
-//                    }
+                
+                
+                //                // 수현 테스트용
+                //                SoohyunTest()
+                //                    .tabItem {
+                //                        Image(systemName: "star")
+                //                        Text("실험용")
+                //                    }
             }
             
-//            if UserDefaults.standard.object(forKey: "userIdToken") != nil {
-//                TabView(selection: $tabSelection) {
-//                    HomeView()
-//                        .tabItem {
-//                            Image(systemName: "house.fill")
-//                            Text("별킷")
-//                        }.tag(0)
-//                    BucketListView()
-//                        .tabItem {
-//                            Image(systemName: "checklist.checked")
-//                            Text("리스트")
-//                        }.tag(1)
-//                    AnalyzeView()
-//                        .tabItem {
-//                            Image(systemName: "chart.bar.xaxis")
-//                            Text("분석")
-//                        }.tag(2)
-//                    MyPageView()
-//                        .tabItem {
-//                            Image(systemName: "person.crop.circle.fill")
-//                            Text("마이페이지")
-//                        }.tag(3)
-//                }
-//            }else{
-//
-//                SignInView()
-//            }
-       
-//        }
+            ZStack{
+                
+                Image("LargeStar")
+                    .resizable()
+                    .renderingMode(.original)
+                    .aspectRatio(contentMode: animate ? .fill : .fit)
+                    .frame(width: animate ? nil : 85, height: animate ? nil : 85)
+                
+                    .scaleEffect(animate ? 3 : 1)
+                
+                    .frame(width: UIScreen.main.bounds.width)
+            }
+            .ignoresSafeArea()
+            .onAppear(perform: animateSplash)
+            .opacity(endSplash ? 0 : 1)
+            
+
+        }
+    }
+    
+    
+    func animateSplash(){
+        DispatchQueue.main.asyncAfter(deadline: .now()){
+            withAnimation(Animation.easeOut(duration: 0.45)){
+                animate.toggle()
+            }
+            withAnimation(Animation.easeOut(duration: 0.45)){
+                endSplash.toggle()
+            }
+        }
     }
 }
 
@@ -82,6 +88,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-            ContentView()
+        ContentView()
     }
 }
