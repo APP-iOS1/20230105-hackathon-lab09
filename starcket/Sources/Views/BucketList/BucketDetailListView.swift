bffdf6f0f0c82d93ed74ad92d04afb4cdf0fc46e
jeoneeee <wldus677@gmail.com>
2023-01-06 10:07:05 +0900

    [Feat] 버킷리스트 뷰 기능구현

--- starcket/Sources/Views/BucketList/BucketDetailListView.swift
+++ starcket/Sources/Views/BucketList/BucketDetailListView.swift
@@ -14,6 +14,7 @@ struct BucketDetailListView: View {
     var detailIdList: [String]
     var bucketId: String
     @Binding var year: Int
+    var isCheck: Bool
     
     var body: some View {
         VStack {
@@ -23,27 +24,34 @@ struct BucketDetailListView: View {
                         NavigationLink {
                             
                         } label: {
-                            HStack {
+                            HStack(alignment: .bottom) {
                                 Text(bucket.title)
-                                    .font(.custom("Pretendard-Regular", size: 22))
+                                    .font(.custom("Pretendard-Regular", size: 18))
                                 Spacer()
                                 Text("\(bucket.createdDate)")
-                                    .font(.custom("Pretendard-Regular", size: 18))
+                                    .font(.custom("Pretendard-Regular", size: 12))
                                     .foregroundColor(.gray)
                             }
                             .padding(.horizontal, Screen.maxWidth * 0.06)
-                            .frame(width: Screen.maxWidth * 0.8, height: Screen.maxHeight * 0.09, alignment: .leading)
+                            .frame(width: Screen.maxWidth * 0.87, height: Screen.maxHeight * 0.09, alignment: .leading)
                             .background {
                                 Color("cellColor")
                             }
                             .cornerRadius(20)
                         }
+                        .padding(.top, Screen.maxWidth * 0.07)
+                        
                         
                     }
                     
                 }
 
-            } // ScrollView
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background {
+                //                Color(hex: "FDFCED")
+                Color("bgColor")
+            }// ScrollView
             
             Button {
                 bucketStore.isLoading = true
@@ -52,29 +60,32 @@ struct BucketDetailListView: View {
                     (bucketStore.bucket, bucketStore.bucketIdList) = try await bucketStore.fetchBucketByDate(String(year))
                     bucketStore.isLoading = false
                 }
-                dismiss
-                
-
             } label: {
+                if isCheck {
                 Text("별킷 달성 완료")
-                    .padding(.horizontal, Screen.maxHeight*0.09)
-                    .font(.custom("Pretendard-Regular", size: 18))
-                    .padding(.vertical, Screen.maxWidth*0.05)
+                } else {
+                    Text("별킷 달성 하기")
+                }
             }
+            .padding(.horizontal, Screen.maxHeight*0.1)
+            .font(.custom("Pretendard-Regular", size: 20))
+            .padding(.vertical, Screen.maxWidth*0.05)
             .background(Color("mainColor"))
             .cornerRadius(40)
             .padding()
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing){
-                    
+            .foregroundColor(.black)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing){
+                if !isCheck {
                     NavigationLink {
                         BucketListDetailAddView()
                     } label: {
                         Image(systemName: "plus.circle")
                             .font(.system(size: 22, weight: .semibold))
                     }
-
                 }
+                
             }
         }
         .onAppear {
