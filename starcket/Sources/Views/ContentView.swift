44cdcf02910023021c1708255ff813638d1cc182
GeonhyeongKim <geonhyeong.dev@gmail.com>
2023-01-06 15:37:52 +0900

    [Fix] 해결

--- starcket/Sources/Views/ContentView.swift
+++ starcket/Sources/Views/ContentView.swift
@@ -11,61 +11,58 @@ struct ContentView: View {
 	@State private var tabSelection: Int = 0
 	@State private var darkmodeToggle: Bool = true
 	@ObservedObject var tabbarManager = TabBarManager.shared
-  @State var animate = false
-  @State var endSplash = false
-    
+	@State var animate = false
+	@State var endSplash = false
+	
 	var body: some View {
-  ZStack {
 		ZStack {
 			ZStack {
-				switch tabbarManager.curTabSelection {
-				case .home:
-					StarScrollView(darkmodeToggle: $darkmodeToggle)
-				case .list:
-					BucketListView()
-				case .chart:
-					AnalyzeView()
-				case .profile:
-					MyPageView(darkmodeToggle: $darkmodeToggle)
+				ZStack {
+					switch tabbarManager.curTabSelection {
+					case .home:
+						StarScrollView(darkmodeToggle: $darkmodeToggle)
+					case .list:
+						BucketListView()
+					case .chart:
+						AnalyzeView()
+					case .profile:
+						MyPageView(darkmodeToggle: $darkmodeToggle)
+					}
+				} // ZStack
+				.padding(.bottom, tabbarManager.bottomPadding)
+				
+				if (tabbarManager.showTabBar) {
+					CustomTabView(darkmodeToggle: $darkmodeToggle)
 				}
 			} // ZStack
-			.padding(.bottom, tabbarManager.bottomPadding)
-			
-			if (tabbarManager.showTabBar) {
-				CustomTabView(darkmodeToggle: $darkmodeToggle)
+			.edgesIgnoringSafeArea(.bottom)
+			.preferredColorScheme(darkmodeToggle ? .dark : .light)
+			ZStack{
+				
+				Image("LargeStar")
+					.resizable()
+					.renderingMode(.original)
+					.aspectRatio(contentMode: animate ? .fill : .fit)
+					.frame(width: animate ? nil : 85, height: animate ? nil : 85)
+				
+					.scaleEffect(animate ? 3 : 1)
+				
+					.frame(width: UIScreen.main.bounds.width)
 			}
-		} // ZStack
-		.edgesIgnoringSafeArea(.bottom)
-		.preferredColorScheme(darkmodeToggle ? .dark : .light)
-                ZStack{
-                
-                Image("LargeStar")
-                    .resizable()
-                    .renderingMode(.original)
-                    .aspectRatio(contentMode: animate ? .fill : .fit)
-                    .frame(width: animate ? nil : 85, height: animate ? nil : 85)
-                
-                    .scaleEffect(animate ? 3 : 1)
-                
-                    .frame(width: UIScreen.main.bounds.width)
-            }
-            .ignoresSafeArea()
-            .onAppear(perform: animateSplash)
-            .opacity(endSplash ? 0 : 1)
-   }
-   
-   func animateSplash(){
-        DispatchQueue.main.asyncAfter(deadline: .now()){
-            withAnimation(Animation.easeOut(duration: 0.45)){
-                animate.toggle()
-            }
-            withAnimation(Animation.easeOut(duration: 0.45)){
-                endSplash.toggle()
-            }
-        }
-    }
-}
-
-	static var previews: some View {
-		ContentView()
+			.ignoresSafeArea()
+			.onAppear(perform: animateSplash)
+			.opacity(endSplash ? 0 : 1)
+		}
+		
+		func animateSplash(){
+			DispatchQueue.main.asyncAfter(deadline: .now()){
+				withAnimation(Animation.easeOut(duration: 0.45)){
+					animate.toggle()
+				}
+				withAnimation(Animation.easeOut(duration: 0.45)){
+					endSplash.toggle()
+				}
+			}
+		}
 	}
+}
